f99795347688ed89003fa01c6d62672f203793fa
Saadique <sadiqzufer@gmail.com>
2023-03-13 02:33:19 +0530

    minor changes

--- src/pages/App/Home.js
+++ src/pages/App/Home.js
@@ -67,7 +67,7 @@ function Home() {
                     <div className='mt-7 mx-2'>
 
                         <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Genuine Journalism is</span> Expensive!</h1>
-                        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">This is news aggregator site where we get news from different sources.</p>
 
                     </div>
                     <div className='mt-7 mx-7'>
